bba00fab016edfbf11f8d608f1d1ef23d7153b7d
Zack Dennis <zack@asetniop.com>
2014-06-15 12:20:30 -0700

    Completed Generator1
    Generator1 - which produces word probabilities for pairs as JSON output
    - is done.

--- Generator1.m
+++ Generator1.m
@@ -95,10 +95,11 @@ numlines = size(matrix,1);
 
 readcleanfile = fopen(cleanfile,'r');
 
-wordcounter = 1;
+wordcounter = 0;
 wordcell{1} = 'xxx';
 details.count = 0;
 details.next = '';
+details.previous = '';
 wordstruct.xxx = details;
 
 for x = 1:numlines
@@ -133,6 +134,8 @@ for x = 1:numlines
         end
     end
 end
+
+fclose(readcleanfile)
  
 outputlength = size(wordcell,1);
 wordcountarray = 0;
@@ -151,17 +154,26 @@ consolidated = {'x',1};
 sortedwordcell = {'x'};
 sortedwordcountarray = 0;
 
+
+
 for x = 1:outputlength
     [maxout,maxloc] = max(workingwordcountarray);
     consolidated{x,1} = workingwordcell{maxloc};
     consolidated{x,2} = workingwordcountarray(maxloc);
     sortedwordcell{x} = workingwordcell{maxloc};
     sortedwordcountarray(x) = workingwordcountarray(maxloc);
+    
+    details.count = workingwordcountarray(maxloc);
+    details.next = '';
+    details.previous = '';
+    sortedwordstruct.(workingwordcell{maxloc}) = details;
+    
     workingwordcountarray(maxloc) = [];
     workingwordcell(maxloc) = [];
+    
 end
 
-%write dictionary file with all punctuation removed
+%write dictionary file 
 
 dicfilename = [language,'-dic.csv'];
 dicfile = [pwd,'/',dicfilename]
@@ -173,7 +185,7 @@ for x = 1:outputlength
 end
 fclose(writedicfile)
 
-testout = consolidated;
+testout = sortedwordstruct;
 
 %get number of lines in file
 matrix = dlmread(inputfile);
@@ -209,19 +221,29 @@ for x = 1:numlines
      numwords = size(spacelocations,2)-1;
     
      if(numwords > 0) %skip blank lines
-         for y = 1:numwords-1 
-             word1 = lower(currentline(spacelocations(y)+1:spacelocations(y+1)-1));
-             word2 = lower(currentline(spacelocations(y+1)+1:spacelocations(y+2)-1));
+         for y = 2:numwords-1 
+             word0 = lower(currentline(spacelocations(y-1)+1:spacelocations(y)-1)); % previous word
+             word1 = lower(currentline(spacelocations(y)+1:spacelocations(y+1)-1)); % active word
+             word2 = lower(currentline(spacelocations(y+1)+1:spacelocations(y+2)-1)); % next word
+             
+             phrase = [word0,word1,word2];
              
              try
-                 test = wordstruct.(word1);
+                 test = sortedwordstruct.(word0);
+                 wordtest0 = 1;
+             catch
+                 wordtest0 = 0;
+             end
+             
+             try
+                 test = sortedwordstruct.(word1);
                  wordtest1 = 1;
              catch
                  wordtest1 = 0;
              end
              
              try
-                 test = wordstruct.(word2);
+                 test = sortedwordstruct.(word2);
                  wordtest2 = 1;
              catch
                  wordtest2 = 0;
@@ -230,10 +252,10 @@ for x = 1:numlines
             
              if wordtest1 > 0 && wordtest2 > 0 %both are valid words
                  following = word2;
-                 pairs = pairs + 1;
                  
-                 details = wordstruct.(word1);
+                 details = sortedwordstruct.(word1);
                  next = details.next;
+                 previous = details.previous;
                  try
                      test = next.(word2);
                      wordtestnext = 1;
@@ -242,18 +264,18 @@ for x = 1:numlines
                  end
                  
                  if wordtestnext > 0 %already an entry
-                     details = wordstruct.(word1);
+                     details = sortedwordstruct.(word1);
                      count = next.(word2);
                      count = count + 1;
                      next.(word2) = count;
                      details.next = next;
-                     wordstruct.(word1) = details;
+                     sortedwordstruct.(word1) = details;
                      
                  else
-                     details = wordstruct.(word1);
+                     details = sortedwordstruct.(word1);
                      next.(word2) = 1;
                      details.next = next;
-                     wordstruct.(word1) = details;
+                     sortedwordstruct.(word1) = details;
                  end
                  
                  
@@ -262,6 +284,43 @@ for x = 1:numlines
                  unfoundwords = unfoundwords + 1;
                  unfound = [word1,'-',word2];
              end
+             
+             if wordtest1 > 0 && wordtest0 > 0 %both are valid words
+                 following = word0;
+                 
+                 details = sortedwordstruct.(word1);
+                 next = details.next;
+                 previous = details.previous;
+                 try
+                     test = previous.(word0);
+                     wordtestprevious = 1;
+                 catch
+                     wordtestprevious = 0;
+                 end
+                 
+                 if wordtestprevious > 0 %already an entry
+                     details = sortedwordstruct.(word1);
+                     count = previous.(word0);
+                     count = count + 1;
+                     previous.(word0) = count;
+                     details.previous = previous;
+                     sortedwordstruct.(word1) = details;
+                     
+                 else
+                     details = sortedwordstruct.(word1);
+                     previous.(word0) = 1;
+                     details.previous = previous;
+                     sortedwordstruct.(word1) = details;
+                 end
+                 
+                 
+             else
+                 following = 'word not found';
+                 unfoundwords = unfoundwords + 1;
+                 unfound = [word1,'-',word2];
+             end
+             
+             
             
         end
     end
@@ -269,18 +328,209 @@ end
 
 pairs = pairs
 unfoundwords = unfoundwords
- 
-testout = wordstruct;
 
 
+%sort structure so words and counts are in descending order
+
+allwords = fieldnames(sortedwordstruct);
+
+allwordssize = size(allwords,1);
+
+for x = 1:allwordssize
+    checkedword = allwords{x};
+    details = sortedwordstruct.(checkedword);
+    count = details.count;
+    nextfields = details.next;
+    allnexts = fieldnames(nextfields);
+    allnextsize = size(allnexts,1);
+    previousfields = details.previous;
+    
+    nextcount = 0; 
+    for y = 1:allnextsize
+         nextword = allnexts{y};
+         nextcount(y,1) = nextfields.(nextword);
+    end
+    
+    %sort output arrays into finished cell array
+    workingnextcount = nextcount;
+    workingallnexts = allnexts;
+    outputcounter = 1;
+    
+    if workingnextcount > 0
+        for x = 1:allnextsize
+            [maxout,maxloc] = max(workingnextcount);
+            revised.(workingallnexts{maxloc}) = maxout;
+            
+            workingallnexts(maxloc) = [];
+            workingnextcount(maxloc) = [];
+        end
+        clear details
+        
+        details.next = revised;
+        details.count = count;
+        details.previous = previousfields;
+        sortedwordstruct.(checkedword) = details; 
+        
+        clear revised;
+    end
+    
+    previousfields = details.previous;
+    allprevious = fieldnames(previousfields);
+    allprevioussize = size(allprevious,1);
+    nextfields = details.next;
+    
+    previouscount = 0; 
+    for y = 1:allprevioussize
+         previousword = allprevious{y};
+         previouscount(y,1) = previousfields.(previousword);
+    end
+    
+    %sort output arrays into finished cell array
+    workingpreviouscount = previouscount;
+    workingallprevious = allprevious;
+    outputcounter = 1;
+    
+    if workingpreviouscount > 0
+        for x = 1:allprevioussize
+            [maxout,maxloc] = max(workingpreviouscount);
+            revised.(workingallprevious{maxloc}) = maxout;
+            
+            workingallprevious(maxloc) = [];
+            workingpreviouscount(maxloc) = [];
+        end
+        clear details
+        
+        details.previous = revised;
+        details.count = count;
+        details.next = nextfields;
+        sortedwordstruct.(checkedword) = details; 
+        
+        clear revised;
+    end
+    
+    
+end
 
 
+%create file with all punctuation removed
+
+jsonfilename = [language,'-predictive.json'];
+jsonfile = [pwd,'/',jsonfilename]
+
+writejsonfile = fopen(jsonfile,'w');
 
 
+print = ['{'];
+fprintf(writejsonfile,'%s\n',print);
+
+%limits to output
+allwordsmax = 100000;
+nextwordsmax = 20;
+previouswordsmax = 20;
 
 
-outputfilename = ['pred-',language,'.json'];
+if allwordssize > allwordsmax
+    allwordssize = allwordsmax;
+end
 
-outputfile = [pwd,'/',outputfilename]
+for x = 1:allwordssize
+    word = allwords{x};
+    details = sortedwordstruct.(word);
+    count = details.count;
+    
+    printword = ['"',word,'":{'];
+    fprintf(writejsonfile,'%s\n',printword);
+    
+    printcount = ['   "count":',num2str(count),','];
+    fprintf(writejsonfile,'%s\n',printcount);
+    
+    
+    print = ['   "next":{'];
+    fprintf(writejsonfile,'%s\n',print);
+    print = ['      '];
+    fprintf(writejsonfile,'%s',print);
+    
+    nextfields = details.next;
+    allnexts = fieldnames(nextfields);
+    allnextsize = size(allnexts,1);
+    previousfields = details.previous;
+    
+    if allnextsize == 0 % closes with no action
+         fprintf(writejsonfile,'%s\n','},');
+    else
+        if allnextsize > nextwordsmax
+            allnextsize = nextwordsmax;
+        end
+        nextcount = 0; 
+        for y = 1:allnextsize
+             nextword = allnexts{y};
+             nextcount = num2str(nextfields.(nextword));
+             printnext = ['"',nextword,'":',nextcount];
+             fprintf(writejsonfile,'%s',printnext);
+             
+             if y == allnextsize
+                 fprintf(writejsonfile,'%s\n','');
+                 fprintf(writejsonfile,'%s\n','      },'); %close with comma for next
+             else
+                 fprintf(writejsonfile,'%s',',');
+             end
+             
+        end
+    end
+    
+    
+    print = ['   "previous":{'];
+    fprintf(writejsonfile,'%s\n',print);
+    print = ['      '];
+    fprintf(writejsonfile,'%s',print);
+    
+    previousfields = details.previous;
+    allprevious = fieldnames(previousfields);
+    allprevioussize = size(allprevious,1);
+    previousfields = details.previous;
+    
+    if allprevioussize == 0 % closes with no action
+         fprintf(writejsonfile,'%s\n','}');
+    else
+        if allprevioussize > previouswordsmax
+            allprevioussize = previouswordsmax;
+        end
+        previouscount = 0; 
+        for y = 1:allprevioussize
+             previousword = allprevious{y};
+             previouscount = num2str(previousfields.(previousword));
+             printprevious = ['"',previousword,'":',previouscount];
+             fprintf(writejsonfile,'%s',printprevious);
+             
+             if y == allprevioussize
+                 fprintf(writejsonfile,'%s\n','');
+                 fprintf(writejsonfile,'%s\n','      }'); % no comma for previous
+             else
+                 fprintf(writejsonfile,'%s',',');
+             end
+             
+        end
+    end
+    
+    if x == allwordssize
+        print = ['   }'];
+    else
+        print = ['   },'];
+    end
+    fprintf(writejsonfile,'%s\n',print);
+    
+    
+end
+
+print = ['}'];
+fprintf(writejsonfile,'%s\n',print);
+
+
+fclose(writejsonfile);
+fclose(readfile);
+
+testout = sortedwordstruct;
+
+
 
 toc
